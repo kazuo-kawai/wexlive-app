f562a4391e0bcbd1cdab591c231cb29af3704737
kazuo.kawai <kazuo.kawai@gmail.com>
2020-11-15 15:07:27 +0900

    add downloads

--- src/views/Download.vue
+++ src/views/Download.vue
@@ -3,15 +3,18 @@
     <h2>ダウンロード</h2>
       <p>Androidバージョン</p>
       <div style="margin:20px 15px;">
-        <ul style="list-style: none;">
-        <li><a href="../downloads/app-hiltonUF2200.apk">HILTON 検品チェック</a></li> 
-        <li><a href="../downloads/app-assetUF2200.apk">アセットワールド 検品チェック</a></li> 
-        <li><a href="../downloads/app-centricUF2200.apk">セントリック銀座 検品チェック</a></li> 
-        <li><a href="../downloads/app-centriccountUF2200.apk">セントリック銀座 洗濯回数チェック</a></li> 
-        <li><a href="../downloads/app-hoshinoyaUF2200.apk">星のや東京 検品チェック</a></li> 
-        <li><a href="../downloads/app-hoshinoyacountUF2200.apk">星のや東京 洗濯回数チェック</a></li> 
-        <li><a href="../downloads/app-tokyobayUF2200.apk">東京ベイ 検品チェック</a></li> 
-        <li><a href="../downloads/app-hyattUF2200.apk">ハイアットプレイス 検品チェック</a></li> 
+        <ul style="list-style: none;text-align:center">
+          <li><a href="../downloads/app-kahalaHotelYokohamaUF2200-release.apk">ザ・カハラ・ホテル＆リゾート横浜</a></li>
+          <li><a href="../downloads/app-interContinentalTKUF2200-release.apk">インターコンチネンタル東京ベイ</a></li> 
+          <li><a href="../downloads/app-mesmUF2200-release.apk">メズム東京オートグラフコレクション</a></li>
+          <li><a href="../downloads/app-yokohamaBayCourtClubUF2200-release.apk">横浜ベイコート倶楽部</a></li>
+          <li><a href="../downloads/app-ritzCarltonNikkoUF2200-release.apk">ザ・リッツ・カールトン日光</a></li>
+          <li><a href="../downloads/app-tokyoEditionToranomonUF2200-release.apk">東京エディション虎ノ門</a></li>
+          <li><a href="../downloads/app-JWMarriottNaraUF2200-release.apk">JWマリオットホテル奈良</a></li>
+          <li><a href="../downloads/app-grandNikkoTKDaibaUF2200-release.apk">グランドニッコー東京台場</a></li>
+          <li><a href="../downloads/app-hyattUF2200.apk">ハイアットプレイス</a></li> 
+          <li><a href="../downloads/app-centricUF2200.apk">ハイアットセントリック銀座東京</a></li> 
+          <li><a href="../downloads/app-hoshinoyaUF2200.apk">星のや東京</a></li> 
         </ul> 
       </div>
     <div class="copy">Copyright &copy; SHIN-NIPPON WEX CO.,LTD All Rights Reserved.</div>
